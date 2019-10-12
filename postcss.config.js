1193fdd019e49209a216b628b6a33412b61b3538
sereneblue <sereneblue@users.noreply.github.com>
2019-10-11 20:10:41 -0400

    add colors to tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
-  content: [
-    './src/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.jsx',
-    // etc.
-  ],
+  content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#6b8e23',
+        'primary-soft': '#7aa329',
+        dark: '#33313b',
+        'dark-softer': '#403E48',
+        light: '#fbfbfb',
+        'light-darker': '#C8C8C8',
+      },
+    },
   },
   variants: {},
   plugins: [require('@tailwindcss/custom-forms')],
