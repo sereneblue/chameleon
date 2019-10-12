ed636cd311dd5270cd0173e9df844a22e8bd3e73
sereneblue <sereneblue@users.noreply.github.com>
2019-10-12 13:22:33 -0400

    update popup classes

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -20,62 +20,64 @@
         <feather type="settings" size="1.5em"></feather>
       </div>
     </div>
-    <div v-show="currentTab == 'home'" class="flex-grow flex-col justify-around">
-      <div class="text-center">
-        <div class="my-6 h-24">
-          <div class="inline-block cursor-pointer" @click="toggleChameleon">
-            <feather v-if="config.enabled" type="shield" class="text-primary" size="6em" stroke-width="2" />
-            <feather v-else type="shield-off" class="text-red-500" size="6em" stroke-width="2" />
-          </div>
-        </div>
-        <div class="text-2xl mb-2 font-regular" :class="[theme.text]">Chameleon is {{ config.enabled ? 'enabled' : 'disabled' }}</div>
-        <div class="flex justify-center text-sm">
-          <div @click="toggleTheme" class="rounded-lg cursor-pointer mr-2" :class="[darkMode ? 'bg-gray-700' : 'bg-gray-300', theme.text]">
-            <div class="flex items-center px-2 py-1">
-              <feather v-if="darkMode" type="moon" size="1.5em"></feather>
-              <feather v-else type="sun" size="1.5em"></feather>
-              <span class="ml-1">{{ darkMode ? 'Dark' : 'Light' }}</span>
+    <div class="flex-grow flex-col justify-around">
+      <div v-if="currentTab == 'home'">
+        <div class="text-center mt-16">
+          <div class="my-6 h-24">
+            <div class="inline-block cursor-pointer" @click="toggleChameleon">
+              <feather v-if="config.enabled" type="shield" class="text-primary" size="6em" stroke-width="2" />
+              <feather v-else type="shield-off" class="text-red-500" size="6em" stroke-width="2" />
             </div>
           </div>
-          <div @click="toggleNotifications" class="rounded-lg cursor-pointer" :class="[darkMode ? 'bg-gray-700' : 'bg-gray-300', theme.text]">
-            <div class="flex items-center px-2 py-1 ">
-              <feather v-if="config.notificationsEnabled" type="bell" size="1.5em"></feather>
-              <feather v-else type="bell-off" size="1.5em"></feather>
-              <span class="ml-1">Notifications</span>
+          <div class="text-2xl mb-2 font-thin" :class="[theme.text]">Chameleon is {{ config.enabled ? 'enabled' : 'disabled' }}</div>
+          <div class="flex justify-center text-sm">
+            <div @click="toggleTheme" class="rounded-lg cursor-pointer mr-2" :class="[darkMode ? 'bg-gray-700' : 'bg-gray-300', theme.text]">
+              <div class="flex items-center px-2 py-1">
+                <feather v-if="darkMode" type="moon" size="1.5em"></feather>
+                <feather v-else type="sun" size="1.5em"></feather>
+                <span class="ml-1">{{ darkMode ? 'Dark' : 'Light' }}</span>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="text-center px-4 py-8">
-          <div class="text-xs uppercase tracking-widest" :class="[theme.text]">Current Profile</div>
-          <div class="text-lg" :class="[theme.text]">Android &middot; Firefox 77</div>
-          <div class="text-lg" :class="[theme.text]">1366x768</div>
-          <div class="text-lg" :class="[theme.text]">Europe/Berlin</div>
-          <div class="inline-block mx-auto rounded-lg cursor-pointer px-2 py-1 my-1" :class="[darkMode ? 'bg-gray-700' : 'bg-gray-300', theme.text]">
-            <div class="flex items-center">
-              <feather type="repeat" size=".8em"></feather>
-              <span class="ml-1 text-xs">change</span>
+            <div @click="toggleNotifications" class="rounded-lg cursor-pointer" :class="[darkMode ? 'bg-gray-700' : 'bg-gray-300', theme.text]">
+              <div class="flex items-center px-2 py-1 ">
+                <feather v-if="config.notificationsEnabled" type="bell" size="1.5em"></feather>
+                <feather v-else type="bell-off" size="1.5em"></feather>
+                <span class="ml-1">Notifications</span>
+              </div>
             </div>
           </div>
         </div>
-        <div class="absolute bottom-0 py-2" :class="[darkMode ? 'bg-dark-softer' : 'bg-gray-200', theme.text]" style="width: -moz-available;">
-          <div class="text-center text-sm uppercase mb-2 tracking-wider">on this page</div>
-          <div class="flex w-full justify-around">
-            <div class="">
-              <feather type="headphones" size="1.5em"></feather>
+        <div>
+          <div class="text-center px-4 py-8">
+            <div class="text-xs uppercase tracking-widest" :class="[theme.text]">Current Profile</div>
+            <div class="text-lg" :class="[theme.text]">Android &middot; Firefox 77</div>
+            <div class="text-lg" :class="[theme.text]">1366x768</div>
+            <div class="text-lg" :class="[theme.text]">Europe/Berlin</div>
+            <div class="inline-block mx-auto rounded-lg cursor-pointer px-2 py-1 my-1" :class="[darkMode ? 'bg-gray-700' : 'bg-gray-300', theme.text]">
+              <div class="flex items-center">
+                <feather type="repeat" size=".8em"></feather>
+                <span class="ml-1 text-xs">change</span>
+              </div>
             </div>
-            <div class="">
-              <feather type="box" size="1.5em"></feather>
-            </div>
-            <div class="">
-              <feather type="calendar" size="1.5em"></feather>
-            </div>
-            <div class="">
-              <feather type="monitor" size="1.5em"></feather>
-            </div>
-            <div class="">
-              <feather type="wifi" size="1.5em"></feather>
+          </div>
+          <div class="absolute bottom-0 py-2" :class="[darkMode ? 'bg-dark-softer' : 'bg-gray-200', theme.text]" style="width: -moz-available;">
+            <div class="text-center text-sm uppercase mb-2 tracking-wider">on this page</div>
+            <div class="flex w-full justify-around">
+              <div class="">
+                <feather type="headphones" size="1.5em"></feather>
+              </div>
+              <div class="">
+                <feather type="box" size="1.5em"></feather>
+              </div>
+              <div class="">
+                <feather type="calendar" size="1.5em"></feather>
+              </div>
+              <div class="">
+                <feather type="monitor" size="1.5em"></feather>
+              </div>
+              <div class="">
+                <feather type="wifi" size="1.5em"></feather>
+              </div>
             </div>
           </div>
         </div>
