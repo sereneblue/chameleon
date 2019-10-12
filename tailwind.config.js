58e2ee8046898d8ff3cfebcae14cfd01a0f5e2c8
sereneblue <sereneblue@users.noreply.github.com>
2019-10-12 13:17:51 -0400

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 module.exports = {
   theme: {
+    customForms: theme => ({
+      default: {
+        select: {
+          borderColor: theme('colors.gray.500'),
+          '&:focus': {
+            boxShadow: '0 0 0 3px rgba(107, 142, 35, 0.5)',
+            borderColor: '#6b8e23',
+          },
+        },
+        checkbox: {
+          borderColor: theme('colors.gray.500'),
+          '&:focus': {
+            boxShadow: '0 0 0 3px rgba(107, 142, 35, 0.5)',
+            borderColor: '#6b8e23',
+          },
+        },
+      },
+    }),
     extend: {
       colors: {
         primary: '#6b8e23',
