08eb2f52d59b75a54c17286441b4494514e1c7ef
sereneblue <sereneblue@users.noreply.github.com>
2019-10-28 23:16:18 -0400

    update options

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,9 +1,15 @@
 import Vue from 'vue';
+import VueFeather from 'vue-feather';
 import App from './App';
+import store from '../store';
 import '../css/tailwind.css';
+
+Vue.use(VueFeather);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
+
   render: h => h(App),
 });
