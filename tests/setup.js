77c4792f0fc86fbfa73813010ea50e3f8a480f6a
sereneblue <sereneblue@users.noreply.github.com>
2019-12-01 16:20:27 -0500

    add routes to test server

--- tests/setup.js
+++ tests/setup.js
@@ -6,8 +6,12 @@ module.exports = async () => {
 
   app.set('view engine', 'ejs');
   app.set('views', path.join(__dirname, '/server'));
-  app.get('/', (req, res) => {
-    res.render('index');
+  app.get('/', (req, res) => res.render('index'));
+  app.get('/headers', (req, res) => res.send(req.headers));
+  app.get('/referer', (req, res) => res.render('index'));
+  app.get('/etag', (req, res) => {
+    res.setHeader('Etag', Math.random().toString(36));
+    res.send('OK');
   });
 
   global.__SERVER1__ = app.listen(3000);
