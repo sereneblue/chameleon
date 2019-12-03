67f693b740d98765f19bf647057ca16ac551154d
sereneblue <sereneblue@users.noreply.github.com>
2019-12-02 22:39:47 -0500

    fix accept-language value

--- src/lib/intercept.ts
+++ src/lib/intercept.ts
@@ -199,7 +199,7 @@ class Interceptor {
           if (this.settings.headers.spoofAcceptLang.enabled) {
             if (this.settings.headers.spoofAcceptLang.value === 'ip') {
               details.requestHeaders[i].value = lang.getLanguage(this.tempStore.ipInfo.lang).value;
-            } else {
+            } else if (this.settings.headers.spoofAcceptLang.value !== 'default') {
               details.requestHeaders[i].value = lang.getLanguage(this.settings.headers.spoofAcceptLang.value).value;
             }
           }
