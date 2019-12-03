52ee0b3067e56afc1bad1ed12942c70f40f7c04a
sereneblue <sereneblue@users.noreply.github.com>
2019-12-02 22:40:29 -0500

    add cors headers to test server

--- tests/setup.js
+++ tests/setup.js
@@ -6,6 +6,11 @@ module.exports = async () => {
 
   app.set('view engine', 'ejs');
   app.set('views', path.join(__dirname, '/server'));
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
   app.get('/', (req, res) => res.render('index'));
   app.get('/headers', (req, res) => res.send(req.headers));
   app.get('/referer', (req, res) => res.render('index'));
