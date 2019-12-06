df29fae0c67866d6b66c9ff0ea1edaa5523c4a04
sereneblue <sereneblue@users.noreply.github.com>
2019-12-05 20:00:57 -0500

    update icon

--- src/lib/webext.ts
+++ src/lib/webext.ts
@@ -3,11 +3,11 @@ let enableChameleon = (enabled: boolean): void => {
   browser.runtime.getPlatformInfo().then(plat => {
     if (enabled === false && plat.os != 'android') {
       browser.browserAction.setIcon({
-        path: '../icons/icon_disabled_48.png',
+        path: '../icons/icon_disabled.svg',
       });
     } else {
       browser.browserAction.setIcon({
-        path: '../icons/icon_48.png',
+        path: '../icons/icon.svg',
       });
     }
   });
