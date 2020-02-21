1974aca09bb9804ac425137ee4c2fd06c0e0706d
sereneblue <sereneblue@users.noreply.github.com>
2020-02-20 22:54:32 -0500

    update webpack config, fix version handling

--- webpack.config.js
+++ webpack.config.js
@@ -93,7 +93,6 @@ const config = {
         to: 'manifest.json',
         transform: content => {
           const jsonContent = JSON.parse(content);
-          jsonContent.version = version;
 
           if (config.mode === 'development') {
             jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval'; object-src 'self'";
