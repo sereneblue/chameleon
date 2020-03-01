76ea4a7eb0e899ad58ab5ed6ce3a7ee41ad835b1
sereneblue <sereneblue@users.noreply.github.com>
2020-03-01 14:54:11 -0500

    fix(interceptor): add fallback url for network request matching

--- src/lib/intercept.ts
+++ src/lib/intercept.ts
@@ -127,7 +127,7 @@ class Interceptor {
     if (!this.settings.config.enabled) return;
 
     // don't modify request for recaptcha
-    if ((details.documentUrl ? details.documentUrl : details.originUrl).startsWith('https://www.google.com/recaptcha/api2')) return;
+    if ((details.originUrl || details.documentUrl || details.url).startsWith('https://www.google.com/recaptcha/api2')) return;
 
     let wl: WhitelistResult = this.checkWhitelist(details);
 
