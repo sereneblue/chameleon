16a8521ed2f6ec03bfd36c1315f23819c05a85d2
sereneblue <sereneblue@users.noreply.github.com>
2020-02-29 23:53:32 -0500

    chore: remove service worker

--- rollup.config.js
+++ rollup.config.js
@@ -80,21 +80,5 @@ export default {
 		),
 
 		onwarn,
-	},
-
-	serviceworker: {
-		input: config.serviceworker.input(),
-		output: config.serviceworker.output(),
-		plugins: [
-			resolve(),
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			commonjs(),
-			!dev && terser()
-		],
-
-		onwarn,
 	}
 };
