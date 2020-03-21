c1306ba2af9b8b5df77973a3121a42b4db024bd9
sereneblue <sereneblue@users.noreply.github.com>
2020-03-21 17:50:44 -0400

    refactor: update layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -4,19 +4,9 @@
 	export let segment;
 </script>
 
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
-
-<Nav {segment}/>
-
 <main>
-	<slot></slot>
+	<div class="flex flex-col h-screen overflow-y-auto">
+		<Nav {segment}/>
+		<slot></slot>
+	</div>
 </main>
