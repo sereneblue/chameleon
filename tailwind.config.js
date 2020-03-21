d4811b216b458e4ffdb6c5fa174245ace4aa0cbd
sereneblue <sereneblue@users.noreply.github.com>
2020-03-21 13:00:48 -0400

    chore: add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#6b8e23',
+        'primary-soft': '#7aa329',
+        'light': '#f7fafc'
+      }
+    },
   },
   variants: {},
   plugins: [],
