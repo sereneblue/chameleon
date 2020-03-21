66b0b85b54bed24bb20191e41a3926aa613a7c26
sereneblue <sereneblue@users.noreply.github.com>
2020-03-21 16:20:24 -0400

    feat: add carousel for landing page

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,54 @@
+<script>
+  let carouselIndex = 0;
+  let screenshots = [
+    "ui1.png",
+    "ui2.png",
+    "ui3.png",
+    "ui4.png",
+    "ui5.png"
+  ];
+  
+  $: src = screenshots[carouselIndex];
+  
+  let interval = setInterval(() => {
+    if (carouselIndex < screenshots.length - 1) {
+      carouselIndex++;
+    } else {
+      carouselIndex = 0;
+    }
+  }, 3000);
+ 
+  let selectImage = (index) => {
+    carouselIndex = index;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      if (carouselIndex < screenshots.length - 1) {
+        carouselIndex++;
+      } else {
+        carouselIndex = 0;
+      }
+    }, 3000);
+  }
+</script>
+
+<style lang="postcss">
+  button {
+    @apply block w-4 h-4 p-0 border-none rounded-full bg-black;
+    opacity: 15%;
+  }
+
+  button.active {
+    @apply bg-primary opacity-100;
+  }
+</style>
+
+<div>
+  <img {src} class="shadow-lg rounded-lg border border-primary b-1" alt="Chameleon screenshot">
+  <div class="flex justify-center mt-6">
+    {#each screenshots as s, i}
+      <button class="mr-2" class:active="{ carouselIndex == i }" on:click={e => selectImage(i) }></button>
+    {/each}
+  </div>
+</div>
