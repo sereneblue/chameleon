e9607c1690c2197d7a07785b6bcabe6649d13569
sereneblue <sereneblue@users.noreply.github.com>
2020-03-21 17:24:57 -0400

    fix: set correct env for production build

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-const production = !process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === 'production';
 const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./src/**/*.svelte", "./src/**/*.html"],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
