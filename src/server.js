78dee35007701bca6aca92bbc297c2e869f07304
sereneblue <sereneblue@users.noreply.github.com>
2020-03-22 00:24:50 -0400

    fix: update base path

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'chameleon',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
