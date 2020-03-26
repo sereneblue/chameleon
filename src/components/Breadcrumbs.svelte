cc1086cc4b8face32a5b026594cdee4d58a678c6
sereneblue <sereneblue@users.noreply.github.com>
2020-03-25 21:37:40 -0400

    feat: add breadcrumb component

---
+++ src/components/Breadcrumbs.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let segment;
+  
+  let capitalize = (word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+</script>
+
+<div class="font-semibold text-lg">
+  {#if segment == undefined  }
+    <span>Home</span>
+  {:else}
+    <a class="text-primary" href="wiki">Home</a>
+    <span class="font-normal mx-1">/</span>
+    <span>{ segment.split('-').map(capitalize).join(' ') }</span>
+  {/if}
+</div>
