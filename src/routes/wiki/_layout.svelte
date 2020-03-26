d499cd854e1f715d492490ae04fb411f95bbd80f
sereneblue <sereneblue@users.noreply.github.com>
2020-03-25 21:59:16 -0400

    feat: add wiki page

---
+++ src/routes/wiki/_layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Nav from '../../components/Nav.svelte';
+	import Breadcrumbs from '../../components/Breadcrumbs.svelte';
+
+	export let segment;
+</script>
+
+<style lang="postcss">
+	:global(p > a) {
+		@apply text-primary underline;
+	}
+</style>
+
+<main>
+	<div class="flex bg-ligh h-full w-full px-2 lg:	px-0 justify-center">
+		<div class="flex lg:max-w-5xl mt-8 w-full">
+			<div class="flex flex-col w-full">
+				<div class="w-full">
+					<div class="mb-5 text-center">
+						<h2 class="font-bold text-5xl">Wiki</h2>
+						<Breadcrumbs {segment} />
+					</div>
+					<slot></slot>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
---
+++ src/routes/wiki/index.svelte
@@ -0,0 +1,62 @@
+<svelte:head>
+	<title>Wiki | Chameleon</title>
+</svelte:head>
+
+<div class="mb-8 w-full leading-snug">
+  <div class="flex flex-col md:flex-row justify-around">
+    <a class="md:w-2/5 mx-4 md:mx-0 hover:text-primary hover:shadow-lg bg-white shadow-md px-6 py-4 rounded mb-4" href="wiki/debug">
+      <span class="font-semibold text-2xl">Debug</span>
+      <p class="text-lg mt-2">How to debug issues with sites while using Chameleon</p>
+    </a>
+    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/developer-guide">
+      <span class="font-semibold text-2xl">Developer Guide</span>
+      <p class="text-lg mt-2">How to programatically control Chameleon</p>
+    </a>
+  </div>
+  <div class="flex flex-col md:flex-row justify-around">
+    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/getting-started">
+      <span class="font-semibold text-2xl">Getting Started</span>
+      <p class="text-lg mt-2">A quick start guide for using Chameleon</p>
+    </a>
+    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/headers">
+      <span class="font-semibold text-2xl">Headers</span>
+      <p class="text-lg mt-2">Learn more about the features on the Headers tab</p>
+    </a>
+  </div>
+  <div class="flex flex-col md:flex-row justify-around">
+    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/ip-rules">
+      <span class="font-semibold text-2xl">IP Rules</span>
+      <p class="text-lg mt-2">Learn more about the IP Rules feature</p>
+    </a>
+    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/options">
+      <span class="font-semibold text-2xl">Options</span>
+      <p class="text-lg mt-2">Learn more about the features on the Options tab</p>
+    </a>
+  </div>
+  <div class="flex flex-col md:flex-row justify-around">
+    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/permissions">
+      <span class="font-semibold text-2xl">Permissions</span>
+      <p class="text-lg mt-2">What permissions does Chameleon require?</p>
+    </a>
+    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/profile">
+      <span class="font-semibold text-2xl">Profile</span>
+      <p class="text-lg mt-2">Learn more about the features on the Profile tab</p>
+    </a>
+  </div>
+  <div class="flex flex-col md:flex-row justify-around">
+    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/recommended-extensions">
+      <span class="font-semibold text-2xl">Recommended Extensions</span>
+      <p class="text-lg mt-2">A list of recommended extensions that you should use with Chameleon</p>
+    </a>
+    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/resources">
+      <span class="font-semibold text-2xl">Resources</span>
+      <p class="text-lg mt-2">Additional resources to improve your privacy</p>
+    </a>
+  </div>
+  <div class="flex flex-col md:flex-row justify-around">
+    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/whitelist">
+      <span class="font-semibold text-2xl">Whitelist</span>
+      <p class="text-lg mt-2">Learn more about the features on the Whitelist tab</p>
+    </a>
+  </div>
+</div>
