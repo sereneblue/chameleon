d37de134b6579c1276880e19db86437646df0d50
sereneblue <sereneblue@users.noreply.github.com>
2020-03-29 16:33:38 -0400

    feat(wiki): add recommended extensions page

---
+++ src/routes/wiki/recommended-extensions.svelte
@@ -0,0 +1,74 @@
+<svelte:head>
+	<title>Recommended Extensions | Chameleon</title>
+</svelte:head>
+
+<div class="mb-8 px-4 text-xl leading-tight">
+  <div class="mb-4">
+    Listed below are addons that you should use in addition to Chameleon.
+  </div>
+  <div>
+    <div class="flex items-center mb-4">
+      <img class="h-10 mr-3 text-center" src="https://addons.cdn.mozilla.net/user-media/addon_icons/607/607454-64.png?modified=mcrushed" alt="uBlock Origin Icon">
+      <div>
+        <p>
+          <a href="https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/" class="font-semibold text-2xl">uBlock Origin</a>
+        </p>
+        Block ads and various trackers.
+      </div>
+    </div>
+    <div class="flex items-center mb-4">
+      <img class="h-10 mr-3 text-center" src="https://addons.cdn.mozilla.net/user-media/addon_icons/613/613250-64.png?modified=1529241623" alt="uMatrix Icon">
+      <div>
+        <p>
+          <a href="https://addons.mozilla.org/en-US/firefox/addon/umatrix/" class="font-semibold text-2xl">uMatrix</a>
+        </p>
+        Blocks JS, iframes, CSS, XHR, cookies, etc.
+      </div>
+    </div>
+    <div class="flex items-center mb-4">
+      <img class="h-10 mr-3 text-center" src="https://addons.cdn.mozilla.net/user-media/addon_icons/0/722-64.png?modified=mcrushed" alt="NoScript Icon">
+      <div>
+        <p>
+          <a href="https://addons.mozilla.org/en-US/firefox/addon/noscript/" class="font-semibold text-2xl">NoScript</a> 
+        </p>
+        Blocks JS, prevent XSS and clickjacking attacks.
+      </div>
+    </div>
+    <div class="flex items-center mb-4">
+      <img class="h-10 mr-3 text-center" src="https://addons.cdn.mozilla.net/user-media/addon_icons/534/534930-64.png?modified=1581849561" alt="Canvas Blocker">
+      <div>
+        <p>
+          <a href="https://addons.mozilla.org/en-US/firefox/addon/canvasblocker/" class="font-semibold text-2xl">Canvas Blocker</a>
+        </p>
+        Modify or block your canvas fingerprint.
+      </div>
+    </div>
+    <div class="flex items-center mb-4">
+      <img class="h-10 mr-3 text-center" src="https://addons.cdn.mozilla.net/user-media/addon_icons/229/229918-64.png?modified=mcrushed" alt="HTTPS Everywhere Icon">
+      <div>
+        <p>
+          <a href="https://addons.mozilla.org/en-US/firefox/addon/https-everywhere/" class="font-semibold text-2xl">HTTPS Everywhere</a>
+        </p>
+        Force HTTPS if a website supports it.
+      </div>
+    </div>
+    <div class="flex items-center mb-4">
+      <img class="h-10 mr-3 text-center" src="https://addons.cdn.mozilla.net/user-media/addon_icons/782/782160-64.png?modified=mcrushed" alt="Firefox Multi-Account Containers Icon">
+      <div>
+        <p>
+          <a href="https://addons.mozilla.org/en-US/firefox/addon/multi-account-containers/" class="font-semibold text-2xl">Firefox Multi-Account Containers</a>
+        </p>
+        Isolate cookies by container. Useful to login to multiple websites at the same time.
+      </div>
+    </div>
+    <div class="flex items-center mb-4">
+      <img class="h-10 mr-3 text-center" src="https://addons.cdn.mozilla.net/user-media/addon_icons/888/888648-64.png?modified=mcrushed" alt="Temporary Containers Icon">
+      <div>
+        <p>
+          <a href="https://addons.mozilla.org/en-US/firefox/addon/temporary-containers/?src=search" class="font-semibold text-2xl">Temporary Containers</a>
+        </p>
+        Disposable containers that act similar to a private browsing tab.
+      </div>
+    </div>
+  </div>
+</div>
