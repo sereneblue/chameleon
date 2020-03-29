36494274cbcd8c004a40e4cac269f3a8ae250035
sereneblue <sereneblue@users.noreply.github.com>
2020-03-29 18:08:22 -0400

    style: use single column layout for wiki

--- src/routes/wiki/index.svelte
+++ src/routes/wiki/index.svelte
@@ -3,60 +3,50 @@
 </svelte:head>
 
 <div class="mb-8 w-full leading-snug">
-  <div class="flex flex-col md:flex-row justify-around">
-    <a class="md:w-2/5 mx-4 md:mx-0 hover:text-primary hover:shadow-lg bg-white shadow-md px-6 py-4 rounded mb-4" href="wiki/debug">
+  <div class="flex flex-col justify-around">
+    <a class="mx-0 hover:text-primary hover:shadow-lg bg-white shadow px-6 py-4 rounded mb-3" href="wiki/debug">
       <span class="font-semibold text-2xl">Debug</span>
-      <p class="text-lg mt-2">How to debug issues with sites while using Chameleon</p>
+      <p class="text-lg mt-1">How to debug issues with sites while using Chameleon</p>
     </a>
-    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/developer-guide">
+    <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/developer-guide">
       <span class="font-semibold text-2xl">Developer Guide</span>
-      <p class="text-lg mt-2">How to programmatically control Chameleon</p>
+      <p class="text-lg mt-1">How to programmatically control Chameleon</p>
     </a>
-  </div>
-  <div class="flex flex-col md:flex-row justify-around">
-    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/getting-started">
+    <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/getting-started">
       <span class="font-semibold text-2xl">Getting Started</span>
-      <p class="text-lg mt-2">A quick start guide for using Chameleon</p>
+      <p class="text-lg mt-1">A quick start guide for using Chameleon</p>
     </a>
-    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/headers">
+    <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/headers">
       <span class="font-semibold text-2xl">Headers</span>
-      <p class="text-lg mt-2">Learn more about the features on the Headers tab</p>
+      <p class="text-lg mt-1">Learn more about the features on the Headers tab</p>
     </a>
-  </div>
-  <div class="flex flex-col md:flex-row justify-around">
-    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/ip-rules">
+    <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/ip-rules">
       <span class="font-semibold text-2xl">IP Rules</span>
-      <p class="text-lg mt-2">Learn more about the IP Rules feature</p>
+      <p class="text-lg mt-1">Learn more about the IP Rules feature</p>
     </a>
-    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/options">
+    <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/options">
       <span class="font-semibold text-2xl">Options</span>
-      <p class="text-lg mt-2">Learn more about the features on the Options tab</p>
+      <p class="text-lg mt-1">Learn more about the features on the Options tab</p>
     </a>
-  </div>
-  <div class="flex flex-col md:flex-row justify-around">
-    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/permissions">
+    <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/permissions">
       <span class="font-semibold text-2xl">Permissions</span>
-      <p class="text-lg mt-2">What permissions does Chameleon require?</p>
+      <p class="text-lg mt-1">What permissions does Chameleon require?</p>
     </a>
-    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/profile">
+    <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/profile">
       <span class="font-semibold text-2xl">Profile</span>
-      <p class="text-lg mt-2">Learn more about the features on the Profile tab</p>
+      <p class="text-lg mt-1">Learn more about the features on the Profile tab</p>
     </a>
-  </div>
-  <div class="flex flex-col md:flex-row justify-around">
-    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/recommended-extensions">
+    <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/recommended-extensions">
       <span class="font-semibold text-2xl">Recommended Extensions</span>
-      <p class="text-lg mt-2">A list of recommended extensions that you should use with Chameleon</p>
+      <p class="text-lg mt-1">A list of recommended extensions that you should use with Chameleon</p>
     </a>
-    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/resources">
+    <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/resources">
       <span class="font-semibold text-2xl">Resources</span>
-      <p class="text-lg mt-2">Additional resources to improve your privacy</p>
+      <p class="text-lg mt-1">Additional resources to improve your privacy</p>
     </a>
-  </div>
-  <div class="flex flex-col md:flex-row justify-around">
-    <a class="md:w-2/5 mx-4 md:mx-0 bg-white shadow-md hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-4" href="wiki/whitelist">
+    <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/whitelist">
       <span class="font-semibold text-2xl">Whitelist</span>
-      <p class="text-lg mt-2">Learn more about the features on the Whitelist tab</p>
+      <p class="text-lg mt-1">Learn more about the features on the Whitelist tab</p>
     </a>
   </div>
 </div>
