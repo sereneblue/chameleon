4706b5d1f7f5dcf5c56c3f720351363c2e361a54
sereneblue <sereneblue@users.noreply.github.com>
2020-04-06 22:38:14 -0400

    feat(wiki): add getting started page

---
+++ src/routes/wiki/getting-started.svelte
@@ -0,0 +1,80 @@
+<script>
+  let settings = {
+    casual: `{"config":{"enabled":true,"notificationsEnabled":false,"theme":"light"},"excluded":[],"headers":{"blockEtag":true,"enableDNT":false,"referer":{"disabled":false,"xorigin":1,"trimming":1},"spoofAcceptLang":{"enabled":false,"value":"default"},"spoofIP":{"enabled":false,"option":0,"rangeFrom":"","rangeTo":""}},"ipRules":[],"options":{"cookiePolicy":"reject_trackers","disableWebRTC":false,"firstPartyIsolate":true,"limitHistory":true,"protectKBFingerprint":{"enabled":false,"delay":1},"protectWinName":true,"resistFingerprinting":false,"screenSize":"default","spoofAudioContext":false,"spoofClientRects":false,"timeZone":"default","trackingProtectionMode":"always","webRTCPolicy":"default_public_interface_only","webSockets":"allow_all"},"profile":{"selected":"randomDesktop","interval":{"option":10,"min":1,"max":1}},"version":"0.20.0.3","whitelist":{"enabledContextMenu":true,"defaultProfile":"none","rules":[]}}`,
+    advanced: `{"config":{"enabled":true,"notificationsEnabled":false,"theme":"light"},"excluded":[],"headers":{"blockEtag":true,"enableDNT":false,"referer":{"disabled":false,"xorigin":1,"trimming":1},"spoofAcceptLang":{"enabled":false,"value":"default"},"spoofIP":{"enabled":false,"option":0,"rangeFrom":"","rangeTo":""}},"ipRules":[],"options":{"cookiePolicy":"reject_third_party","disableWebRTC":false,"firstPartyIsolate":true,"limitHistory":true,"protectKBFingerprint":{"enabled":false,"delay":1},"protectWinName":true,"resistFingerprinting":true,"screenSize":"profile","spoofAudioContext":true,"spoofClientRects":true,"timeZone":"default","trackingProtectionMode":"always","webRTCPolicy":"default_public_interface_only","webSockets":"block_3rd_party"},"profile":{"selected":"randomDesktop","interval":{"option":10,"min":1,"max":1}},"version":"0.20.0.3","whitelist":{"enabledContextMenu":true,"defaultProfile":"none","rules":[]}}`
+  };
+
+  let download = (k) => {
+    let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(settings[k]));
+    var el = document.getElementById('dl');
+    el.setAttribute("href", dataStr );
+    el.setAttribute("download", "settings.json");
+    el.click();
+  };
+</script>
+
+<svelte:head>
+	<title>Getting Started | Chameleon</title>
+</svelte:head>
+
+<div class="mb-8 px-4 text-xl leading-snug">
+  <div>
+    <div class="mb-4">
+      <div class="mt-4">
+        <p class="mb-3">
+          Chameleon is an extension to spoof your browser profile. Your browser profile contains information 
+          such as your user agent, screen resolution, accept encoding, and various other bits of information
+          that can be used to track you across the web. To learn more the features Chameleon offers, visit 
+          the guides below.
+        </p>
+        <div class="flex flex-col justify-around mb-4">
+          <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/profile">
+            <span class="font-semibold text-2xl">Profile</span>
+            <p class="text-lg mt-1">Learn more about the features on the Profile tab</p>
+          </a>
+          <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/headers">
+            <span class="font-semibold text-2xl">Headers</span>
+            <p class="text-lg mt-1">Learn more about the features on the Headers tab</p>
+          </a>
+          <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/options">
+            <span class="font-semibold text-2xl">Options</span>
+            <p class="text-lg mt-1">Learn more about the features on the Options tab</p>
+          </a>
+          <a class="mx-0 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" href="wiki/whitelist">
+            <span class="font-semibold text-2xl">Whitelist</span>
+            <p class="text-lg mt-1">Learn more about the features on the Whitelist tab</p>
+          </a>
+        </div>
+        <p class="py-2">
+          Chameleon has a lot of options that you can toggle. For your convenience, here are a few settings 
+          files that you can import into Chameleon to quickly get started.
+        </p>
+        <div class="flex justify-around py-2">
+          <a class="hidden" href="##" id="dl">Download</a>
+          <button class="mx-0 flex items-center w-2/5 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" on:click={e => download('casual')}>
+            <div class="flex flex-col text-left flex-1">
+              <span class="font-semibold text-2xl">Casual</span>
+              <p class="text-lg mt-1">For new users</p>
+            </div>
+            <svg fill="currentColor" viewBox="0 0 20 20" class="w-12 h-12"><path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </button>
+          <button class="mx-0 flex items-center w-2/5 bg-white shadow hover:text-primary hover:shadow-lg px-6 py-4 rounded mb-3" on:click={e => download('advanced')}>
+            <div class="flex flex-col text-left flex-1">
+              <span class="font-semibold text-2xl">Advanced</span>
+              <p class="text-lg mt-1">For intermediate users</p>
+            </div>
+            <svg fill="currentColor" viewBox="0 0 20 20" class="w-12 h-12"><path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </button>
+        </div>
+        <h2 class="font-semibold text-3xl border-primary border-b-2 mb-4">Dashboard</h2>
+        <div class="text-lg mb-4 w-full flex justify-center">
+          <img src="dashboard.png" alt="Chameleon IP Rules">
+        </div>
+        <p class="py-2">
+          The dashboard can be accessed by clicking the gear icon on the Chameleon popup. You can export, import,
+          and reset your settings here. It also provides quick access to helpful resources for the Chameleon community. 
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
