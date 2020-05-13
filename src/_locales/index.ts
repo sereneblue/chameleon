8a7c28f2b3467a05871ee226f06240aef6407969
sereneblue <sereneblue@users.noreply.github.com>
2020-05-13 19:57:04 -0400

    fix(localization): import russian locale

--- src/_locales/index.ts
+++ src/_locales/index.ts
@@ -2,6 +2,7 @@ const en = require('./en/messages.json');
 const es = require('./es/messages.json');
 const de = require('./de/messages.json');
 const fr = require('./fr/messages.json');
+const ru = require('./ru/messages.json');
 const zh_CN = require('./zh_CN/messages.json');
 
 export default {
@@ -9,5 +10,6 @@ export default {
   es,
   de,
   fr,
+  ru,
   zh_CN,
 };
