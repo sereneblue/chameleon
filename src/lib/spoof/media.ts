4bf47660969538440386fa306c9aaf0653f5ed71
sereneblue <sereneblue@users.noreply.github.com>
2020-05-25 17:55:54 -0400

    refactor(injection): improve media devices blocking; #376

--- src/lib/spoof/media.ts
+++ src/lib/spoof/media.ts
@@ -1,8 +1,15 @@
 export default {
-  type: 'custom',
-  data: `
+  type :  'custom',
+  data :  `
   if (navigator.mediaDevices) {
     Object.defineProperty(navigator.mediaDevices, 'enumerateDevices', {
+      configurable: true,
+      value: async () => {
+        return [];
+      }
+    });
+
+    Object.defineProperty(navigator.mediaDevices, 'getUserMedia', {
       configurable: true,
       value: async () => {
         return [];
