bea4d82b71e03a108cc441d072784a7608c23c72
sereneblue <sereneblue@users.noreply.github.com>
2020-09-01 22:07:50 -0400

    fix(language spoofing): update language spoofing to add support for Intl APIs
    Intl.RelativeTimeFormat, Intl.NumberFormat, and Intl.Collator

--- src/lib/spoof/language.ts
+++ src/lib/spoof/language.ts
@@ -13,7 +13,7 @@ export default {
       value: CHAMELEON_SPOOF.get(window).language.nav
     });
 
-    window.Intl.PluralRules = function() {
+    window.Intl.PluralRules = function PluralRules() {
       let args = [...arguments];
 
       if (arguments.length == 0 || !arguments[0]) {
@@ -30,7 +30,7 @@ export default {
     if (window.Intl.ListFormat) {
       var ORIGINAL_INTL_LF = window.Intl.ListFormat;
 
-      window.Intl.ListFormat = function() {
+      window.Intl.ListFormat = function ListFormat() {
         let args = [...arguments];
 
         if (arguments.length == 0 || !arguments[0]) {
@@ -44,6 +44,60 @@ export default {
         window.Intl.ListFormat, "ListFormat"
       ]);
     }
+
+    if (window.Intl.RelativeTimeFormat) {
+      var ORIGINAL_INTL_RTF = window.Intl.RelativeTimeFormat;
+
+      window.Intl.RelativeTimeFormat = function RelativeTimeFormat() {
+        let args = [...arguments];
+
+        if (arguments.length == 0 || !arguments[0]) {
+          args[0] = navigator.language || "en-US";
+        }
+
+        return new (Function.prototype.bind.apply(ORIGINAL_INTL_RTF, [null, ...args]));
+      };
+
+      modifiedAPIs.push([
+        window.Intl.RelativeTimeFormat, "RelativeTimeFormat"
+      ]);
+    }
+
+    if (window.Intl.NumberFormat) {
+      var ORIGINAL_INTL_NF = window.Intl.NumberFormat;
+
+      window.Intl.NumberFormat = function NumberFormat() {
+        let args = [...arguments];
+
+        if (arguments.length == 0 || !arguments[0]) {
+          args[0] = navigator.language || "en-US";
+        }
+
+        return new (Function.prototype.bind.apply(ORIGINAL_INTL_NF, [null, ...args]));
+      };
+
+      modifiedAPIs.push([
+        window.Intl.NumberFormat, "NumberFormat"
+      ]);
+    }
+
+    if (window.Intl.Collator) {
+      var ORIGINAL_INTL_C = window.Intl.Collator;
+
+      window.Intl.Collator = function Collator() {
+        let args = [...arguments];
+
+        if (arguments.length == 0 || !arguments[0]) {
+          args[0] = navigator.language || "en-US";
+        }
+
+        return new (Function.prototype.bind.apply(ORIGINAL_INTL_C, [null, ...args]));
+      };
+
+      modifiedAPIs.push([
+        window.Intl.Collator, "Collator"
+      ]);
+    }
   `
     .replace(
       /ORIGINAL_INTL_PR/g,
@@ -54,6 +108,27 @@ export default {
     )
     .replace(
       /ORIGINAL_INTL_LF/g,
+      String.fromCharCode(65 + Math.floor(Math.random() * 26)) +
+        Math.random()
+          .toString(36)
+          .substring(Math.floor(Math.random() * 5) + 5)
+    )
+    .replace(
+      /ORIGINAL_INTL_RTF/g,
+      String.fromCharCode(65 + Math.floor(Math.random() * 26)) +
+        Math.random()
+          .toString(36)
+          .substring(Math.floor(Math.random() * 5) + 5)
+    )
+    .replace(
+      /ORIGINAL_INTL_NF/g,
+      String.fromCharCode(65 + Math.floor(Math.random() * 26)) +
+        Math.random()
+          .toString(36)
+          .substring(Math.floor(Math.random() * 5) + 5)
+    )
+    .replace(
+      /ORIGINAL_INTL_C/g,
       String.fromCharCode(65 + Math.floor(Math.random() * 26)) +
         Math.random()
           .toString(36)
