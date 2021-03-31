ba1805a86a6adff729e269c4714b0cdffb89cf40
sereneblue <sereneblue@users.noreply.github.com>
2021-03-31 18:53:17 -0400

    feat: update privacy policy

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -7,11 +7,16 @@
 		<div class="flex flex-col w-full">
 			<h2 class="font-bold text-5xl mb-5 text-center">Privacy Policy</h2>
 			<div class="w-full text-2xl px-4">
-        <p>Chameleon doesn't collect any information from users.</p>
-        <p class="mt-4">
-          If you are using a specific feature (language or timezone spoofing), a request may be made to ipapi.co.
-          This functionality is only enabled when the user specfies "IP" for those features. ipapi's privacy policy can be found <a href="https://ipapi.co/privacy/" target="_blank" rel="noopener noreferrer">here</a>.  
-        </p>
+		        <p>Chameleon doesn't collect any information from users.</p>
+		        <div class="mt-4">
+		          <div class="text-center w-full my-4 bg-orange-200 p-2 text-base rounded">
+	          		ipapi.co is no longer used for geolocation as of v0.21.18.<br> A <a href="https://vercel.com/" class="underline" target="_blank" rel="noopener noreferrer">Vercel</a> app now powers IP geolocation.
+	          	  </div>
+	          	  <div>
+			          If you are using a specific feature (language or timezone spoofing), a request may be made to ipapi.co.
+			          This functionality is only enabled when the user specfies "IP" for those features. ipapi's privacy policy can be found <a href="https://ipapi.co/privacy/" target="_blank" rel="noopener noreferrer">here</a>.
+			      </div>
+		        </div>
 			</div>
 		</div>
 	</div>
